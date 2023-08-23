6cd1f29c91c630bd87ab9e0f967a26bd5d271ac5
kelebekkadircan <kelebekkadircan@gmail.com>
2023-08-23 16:21:43 +0300

    nextjs_fetching

---
+++ fetching/pages/index.js
@@ -0,0 +1,61 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import fs from 'fs/promises'
+import path from 'path'
+import { redirect } from 'next/dist/server/api-utils'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home(props) {
+
+  const { products } = props;
+
+
+  return (
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}> {product.title} </li>
+        ))}
+      </ul>
+
+    </>
+  )
+}
+
+export async function getStaticProps() {
+
+  console.log('Re-Generating');
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+
+  const jsonData = await fs.readFile(filePath);
+
+  const data = JSON.parse(jsonData)
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+
+    }
+
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true }
+  }
+
+
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 60,
+
+  }
+}
