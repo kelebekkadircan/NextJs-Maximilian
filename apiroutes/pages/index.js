c82d56f6f819f73402f69db9753a7a85cd15b32d
kelebekkadircan <kelebekkadircan@gmail.com>
2023-09-05 22:56:20 +0300

    api_routes

---
+++ apiroutes/pages/index.js
@@ -0,0 +1,75 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { useRef, useState } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback }
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+
+  }
+
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then(response => response.json())
+      .then(data => setFeedbackItems(data.feedback))
+  }
+
+  return (
+    <>
+      <div>
+        <h1> The Home Page</h1>
+        <form onSubmit={submitFormHandler}>
+
+          <div>
+            <label htmlFor="email">Your Email Address</label>
+            <input type="email" id='email' ref={emailInputRef} />
+          </div>
+
+          <div>
+            <label htmlFor="feedback" >Your Feedback</label>
+            <textarea type="feedback" rows='5' ref={feedbackInputRef} >
+
+            </textarea>
+          </div>
+
+          <button>Send Feedback</button>
+
+        </form>
+        <hr />
+
+        <button onClick={loadFeedbackHandler}>Load Feedback</button>
+
+        <ul>
+          {feedbackItems.map((item) => <li key={item.id} > {item.text} -- {item.email} </li>)}
+        </ul>
+
+      </div>
+
+    </>
+  )
+}
